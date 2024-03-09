6411a7bbda6be838d9383a8cd32140e6ad37bc55
iayakunin <iayakunin@avito.ru>
2024-03-09 10:53:32 +0300

    update task 4

--- lesson_1/task4.py
+++ lesson_1/task4.py
@@ -3,46 +3,72 @@ from tqdm import tqdm
 
 
 def algorithm() -> int:
-    figures = {"R": [], "B": []}
+    figures = {
+        "R": [ # ладья
+            [],
+            [0, -1], [1, 0], [0, 1], [-1, 0] # паттерн перемещения
+        ],
+        "B": [ # слон
+            [],
+            [1, 1], [-1, -1], [1, -1], [-1, 1] # паттерн перемещения
+        ],
+    }
     cnt = 64
     for row in range(8):
         line = list(input().strip())
         while "R" in line:
             column = line.index("R")
-            figures["R"].append([row, column])
+            figures["R"][0].append([row, column])
             line[column] = "*"
         while "B" in line:
             column = line.index("B")
-            figures["B"].append([row, column])
+            figures["B"][0].append([row, column])
             line[column] = "*"
-    if figures == {"R": [], "B": []}:
-        return cnt
-    for row in range(8):
-        for column in range(8):
-            subtract = False
-            for figure, coord in figures.items():
-                for r_fig, c_fig in coord:
-                    x_dif = abs(row - r_fig)
-                    y_dif = abs(column - c_fig)
-                    if figure == "B" and x_dif == y_dif and not subtract:
+
+    coord_figures = figures["R"][0].copy()
+    coord_figures.extend(figures["B"][0])
+    list_coord = []
+
+    for figure, coord_list in figures.items():
+        coords = coord_list[0]
+        doc = coord_list[1:]
+        for x_fig, y_fig in coords:
+            cnt -= 1 # удаляем фигуру которую сейчас исследуем
+            for x_mul, y_mul in doc:
+                step = True
+                step_plus = 1
+                while step:
+                    x_now = x_fig + step_plus * x_mul
+                    y_now = y_fig + step_plus * y_mul
+
+                    if [x_now, y_now] in coord_figures:
+                        step = False
+                        continue
+
+                    if x_now > 7 or x_now < 0 or y_now > 7 or y_now < 0:
+                        step = False
+                    elif [x_now, y_now] in list_coord:
+                        pass
+                    else:
                         cnt -= 1
-                        subtract = True
-                    elif figure == "R" and (x_dif == 0 or y_dif == 0) and not subtract:
-                        cnt -= 1
-                        subtract = True
-    return cnt
+                        list_coord.append([x_now, y_now])
+
+                    # if [x_now, y_now] in coord_figures:
+                    #     step = False
+                    step_plus += 1
+    return cnt, list_coord, coord_figures
 
 
 def test_algorithm(*args):
     with patch("builtins.input", side_effect=[*args[:-1]]):
         test_answer = algorithm()
         print(test_answer)
-        assert test_answer == args[-1]
+        assert test_answer[0] == args[-1]
 
 
 if __name__ == "__main__":
     test_case = [
-        [
+        [ # 1
             "********  ",
             "******** ",
             "*R******     ",
@@ -55,7 +81,7 @@ if __name__ == "__main__":
             "",
             49
         ],
-        [
+        [ # 2
             "********  ",
             "******** ",
             "******B*     ",
@@ -68,7 +94,7 @@ if __name__ == "__main__":
             "",
             54
         ],
-        [
+        [ # 3
             "********  ",
             "*R****** ",
             "********     ",
@@ -80,6 +106,71 @@ if __name__ == "__main__":
             "",
             "",
             40
+        ],
+        [ # 4
+            "R*******  ",
+            "******** ",
+            "********     ",
+            "********   ",
+            "********  ",
+            "********",
+            "********",
+            "********",
+            "",
+            "",
+            49
+        ],
+        [ # 5
+            "B*******  ",
+            "******** ",
+            "********     ",
+            "********   ",
+            "********  ",
+            "********",
+            "********",
+            "********",
+            "",
+            "",
+            56
+        ],
+        [ # 6
+            "********  ",
+            "******** ",
+            "********     ",
+            "****B***   ",
+            "****R***  ",
+            "********",
+            "********",
+            "********",
+            "",
+            "",
+            41
+        ],
+        [ # 7
+            "R*******  ",
+            "*B****** ",
+            "********     ",
+            "********   ",
+            "********  ",
+            "********",
+            "********",
+            "********",
+            "",
+            "",
+            42
+        ],
+        [ # 8
+            "********  ",
+            "*R*****B ",
+            "********     ",
+            "********   ",
+            "********  ",
+            "********",
+            "********",
+            "********",
+            "",
+            "",
+            43 # [1,7]
         ],
     ]
     for case in tqdm(test_case):
