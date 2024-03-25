f4a887da25d4e8112d233fc868805372e31da3a4
iayakunin <yakunin1996@gmail.com>
2024-03-25 21:33:28 +0300

    add solution for task 5 lesson 1

--- l1_complexity_testing/task5.py
+++ l1_complexity_testing/task5.py
@@ -4,7 +4,23 @@ from tqdm import tqdm
 
 
 def algorithm():
-    pass
+    n, k, d = map(int, input().split())
+    nowmod = n % k
+    ans = [n]
+    flag = True
+    for i in range(d):
+        for newdigit in range(10):
+            newmod = (nowmod * 10 + newdigit) % k
+            if newmod == 0:
+                ans.append(newdigit)
+                nowmod = newmod
+                break
+        else:
+            flag = False
+    if flag:
+        return int(''.join(map(str, ans)))
+    else:
+        return -1
 
 
 def test_algorithm(*args):
